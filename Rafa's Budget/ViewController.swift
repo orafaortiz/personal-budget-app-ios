528f57a347a9d485aa7386df65cf3ba26aeaaad3
Rafael <rhb.ortiz@gmail.com>
2021-06-27 12:43:57 -0300

    Initial Commit

---
+++ Rafa's Budget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rafa's Budget
+//
+//  Created by Rafael Ortiz on 27/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rafa's BudgetTests/Rafa_s_BudgetTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rafa_s_BudgetTests.swift
+//  Rafa's BudgetTests
+//
+//  Created by Rafael Ortiz on 27/06/21.
+//
+
+import XCTest
+@testable import Rafa_s_Budget
+
+class Rafa_s_BudgetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
